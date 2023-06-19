c4fe88c620ddc21f749eff786fc74de9a1b869cb
javamogi <40781237+javamogi@users.noreply.github.com>
2023-06-19 17:08:02 +0900

    요구사항 5 - 로그인하기
    로그인 성공하면 쿠키를 저장해야한다 (logined=true)
    Path를 설정하지 않을 경우 /user의 요청 url에만 쿠키가 저장된다

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,12 +5,12 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
-import util.RequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -37,10 +37,14 @@ public class RequestHandler extends Thread {
             int contentLength = 0;
             while(!"".equals(line)){
                 line = br.readLine();
-                log.debug(line);
+//                log.debug(line);
                 if(line.startsWith("Content-Length")){
                     String[] contentLengthArr = line.split(": ");
                     contentLength = Integer.parseInt(contentLengthArr[1]);
+                } else if(line.startsWith("Cookie")){
+                    String[] cookieArr = line.split(": ");
+                    String cookies = cookieArr[1];
+                    log.debug("Cookie : {}", cookies);
                 }
             }
 
@@ -50,17 +54,34 @@ public class RequestHandler extends Thread {
                 Map<String, String> params = HttpRequestUtils.parseQueryString(content);
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("user : {}", user);
+                DataBase.addUser(user);
                 DataOutputStream dos = new DataOutputStream(out);
-                response302Header(dos, "/index.html");
+                response302Header(dos, "/user/login.html");
+            } else if (url.equals("/user/login")){
+                String content = IOUtils.readData(br, contentLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(content);
+                String userId = params.get("userId");
+                String password = params.get("password");
+                User user = DataBase.findUserById(userId);
+                DataOutputStream dos = new DataOutputStream(out);
+                if(user == null || !user.getPassword().equals(password)){
+                    responseResource(dos, "/user/login_failed.html");
+                    return;
+                }
+                responseLoginSuccess(dos, "/index.html");
             } else {
                 DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                responseResource(dos, url);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void responseResource(DataOutputStream dos, String path) throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -74,10 +95,21 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void responseLoginSuccess(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Set-Cookie: logined=true; Path=/ \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void response302Header(DataOutputStream dos, String url) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("Location: " + url + " \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
