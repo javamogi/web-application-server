347c606c12ab5ab03791b615a869edecb7b78762
javamogi <40781237+javamogi@users.noreply.github.com>
2023-06-19 14:25:26 +0900

    요구사항 2 - GET 방식으로 회원가입하기
    내가 생각한 코드
    회원가입 요청 이후 페이지를 찾을 수 없다.

--- src/main/java/util/RequestUtils.java
+++ src/main/java/util/RequestUtils.java
@@ -1,9 +1,27 @@
 package util;
 
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
 public class RequestUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestUtils.class);
 
     public static String getUrl(String line){
         String[] tokens = line.split(" ");
-        return tokens[1];
+        String url = tokens[1];
+        if(url.contains(".html")){
+            return tokens[1];
+        }
+        int index = url.indexOf("?");
+        String requestPath = url.substring(0, index);
+        String params = url.substring(index+1);
+        Map<String, String> values = HttpRequestUtils.parseQueryString(params);
+        User user = new User(values.get("userId"), values.get("password"), values.get("name"), values.get("email"));
+        log.debug("user : {}", user);
+        return requestPath;
     }
 }
--- src/test/java/util/RequestUtilsTest.java
+++ src/test/java/util/RequestUtilsTest.java
@@ -8,8 +8,14 @@ import static org.junit.Assert.*;
 public class RequestUtilsTest {
 
     @Test
-    public void getUrl(){
+    public void getUrl_html_요청(){
         String rq = "GET /index.html HTTP/1.1";
         assertEquals("/index.html", RequestUtils.getUrl(rq));
+    }
+
+    @Test
+    public void getUrl_queryString_요청(){
+        String rq = "GET /user/create?userId=javamogi HTTP/1.1";
+        assertEquals("/user/create", RequestUtils.getUrl(rq));
     }
 }
